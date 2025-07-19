b84b989fb30020d20aef5ff364859ba708d87a14
halongvu208 <halongvu30072008@gmail.com>
2025-07-19 23:45:56 +0000

    hú

---
+++ script.js
@@ -0,0 +1,12 @@
+function dangNhap() {
+      document.getElementById('thongbao').textContent = "👉 Tính năng đăng nhập đang được phát triển.";
+      }
+
+      function dangKy() {
+        document.getElementById('thongbao').textContent = "✅ Bạn đã đăng ký thành công và nhận 20.000đ!";
+        }
+
+        function rutTien() {
+          document.getElementById('thongbao').textContent = "💬 Bạn chỉ có thể rút khi số dư ≥ 50.000đ.";
+          }
+}
